b9791de092289fb3f62ca7adf1388b6117c5e416
Ghosttrio <ghosttrio@naver.com>
2024-09-07 21:07:01 +0900

    first commit

---
+++ src/main/kotlin/com/ghosttrio/clubmanager/ClubManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.ghosttrio.clubmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClubManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClubManagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/ghosttrio/clubmanager/ClubManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ghosttrio.clubmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClubManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
